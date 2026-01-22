a4ee27633a70d56514a32484e0fc4d2e9fe86768
Thiru <thirukumar3210@gmail.com>
2026-01-22 22:26:49 +0530

    changed something

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
 import Favorites from './pages/Favorites'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
